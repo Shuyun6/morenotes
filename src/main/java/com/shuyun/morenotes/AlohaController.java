ea9de1345629866f6986a518d02b975fe072f9f0
shuyun <shuyungogogo@hotmail.com>
2018-10-25 22:37:19 +0800

    add YAML file test some GET and POST requests

--- src/main/java/com/shuyun/morenotes/AlohaController.java
+++ src/main/java/com/shuyun/morenotes/AlohaController.java
@@ -1,7 +1,6 @@
 package com.shuyun.morenotes;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -18,5 +17,46 @@ public class AlohaController {
     public List<String> aloha1(){
         return Arrays.asList("A", "B");
     }
+
+    @RequestMapping(value = "/getUser", method = RequestMethod.GET)
+    public User getPerson(){
+        User user = new User();
+        user.setId((long) 101);
+        user.setName("shuyun");
+        return user;
+    }
+
+    @GetMapping(value = "/getUserByName")
+    public User getPerson(@RequestParam(value = "name") String name){
+        //url should be like: "http://127.0.0.1:8080/api/getUserByName?name=shuyun"
+        User user = new User();
+        user.setName(name);
+        return user;
+    }
+
+    @RequestMapping(value = "/getUserByDesc/{desc}", method = RequestMethod.GET)
+    public User getPersonByTitle(@PathVariable(value = "desc") String des){
+        //url should be like: "http://127.0.0.1:8080/api/getUserByTitle/master"
+        //the param is master
+        User user = new User();
+        user.setDescription(des);
+        return user;
+    }
+
+    @RequestMapping(value = "/getUserById", method = RequestMethod.POST)
+    public User getPersonById(Long id){
+        //url in post request like: http://127.0.0.1:8080/api/getUserById?id=123
+        //or use postmen to send post body
+        User user = new User();
+        user.setId(id);
+        return user;
+    }
+
+    @RequestMapping(value = "/getUserByBody", method = RequestMethod.POST)
+    public User getPersonByPerson(@RequestBody User person){
+        //just send a json body via application/json media type
+        return person;
+    }
+
 
 }
--- src/main/java/com/shuyun/morenotes/User.java
+++ src/main/java/com/shuyun/morenotes/User.java
@@ -1,8 +1,41 @@
 package com.shuyun.morenotes;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "t_user")
 public class User {
 
+    @Id
+    @GeneratedValue
     private Long id;
     private String name;
+    private String description;
 
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
